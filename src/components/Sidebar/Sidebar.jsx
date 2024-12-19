2f503d093bf6c79e320d197121a8bea54f65f885
hardik <hardikpatel1903@gmail.com>
2024-12-19 11:43:28 +0530

    Your commit message here

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({ toggle }) => {
           <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
         </svg>
       <button
-        className="btn dropdown-toggle"
+        className="btn dropdown-toggle fw-normal"
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
@@ -40,16 +40,16 @@ const Sidebar = ({ toggle }) => {
         <a className="nav-link" href="https://example.com">Blog</a>
     </li>
       </ul>
-      <p className="dash-text">APPS</p>
-      <button className="btn dash-span">Calender</button><br/>
+      <p className="text-muted">APPS</p>
+      <button className="btn fw-normal">Calender</button><br/>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
         <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-      </svg><button className="btn btn-text ">Chat</button><br/>
+      </svg><button className="btn btn-text fw-normal">Chat</button><br/>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
       </svg>
       <button
-        className="btn dropdown-toggle"
+        className="btn dropdown-toggle fw-normal"
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
@@ -60,7 +60,7 @@ const Sidebar = ({ toggle }) => {
         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
       </svg>
       <button
-        className="btn dropdown-toggle"
+        className="btn dropdown-toggle fw-normal"
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
@@ -72,7 +72,7 @@ const Sidebar = ({ toggle }) => {
         <path d="M6.5 3a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1zm-4 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1zm8 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1z"/>
       </svg>
       <button
-        className="btn dropdown-toggle"
+        className="btn dropdown-toggle fw-normal"
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
@@ -83,7 +83,7 @@ const Sidebar = ({ toggle }) => {
         <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z"/>
       </svg>
       <button
-        className="btn dropdown-toggle"
+        className="btn dropdown-toggle fw-normal"
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
@@ -94,13 +94,13 @@ const Sidebar = ({ toggle }) => {
         <path d="m.5 3 .04.87a2 2 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2m5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19q-.362.002-.683.12L1.5 2.98a1 1 0 0 1 1-.98z"/>
         <path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5"/>
       </svg>
-      <button class="btn">File Manager</button><br/>
+      <button class="btn fw-normal">File Manager</button><br/>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
         <path d="m.5 3 .04.87a2 2 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2m5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19q-.362.002-.683.12L1.5 2.98a1 1 0 0 1 1-.98z"/>
         <path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5"/>
       </svg>
       <button
-      className="btn dropdown-toggle"
+      className="btn dropdown-toggle fw-normal"
       type="button"
       data-bs-toggle="dropdown"
       aria-expanded="false"
@@ -112,7 +112,7 @@ const Sidebar = ({ toggle }) => {
       <path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5"/>
     </svg>
     <button
-    className="btn dropdown-toggle"
+    className="btn dropdown-toggle fw-normal"
     type="button"
     data-bs-toggle="dropdown"
     aria-expanded="false"
@@ -123,7 +123,7 @@ const Sidebar = ({ toggle }) => {
     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
   </svg>
   <button
-  className="btn dropdown-toggle"
+  className="btn dropdown-toggle fw-normal"
   type="button"
   data-bs-toggle="dropdown"
   aria-expanded="false"
@@ -134,19 +134,19 @@ const Sidebar = ({ toggle }) => {
   <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
 </svg>
 <button
-className="btn dropdown-toggle"
+className="btn dropdown-toggle fw-normal"
 type="button"
 data-bs-toggle="dropdown"
 aria-expanded="false"
 >
 Blog
 </button><br/>
-<p className="dash-text mt-4">LAYOUT & PAGES</p>
+<p className="text-muted mt-4">LAYOUT & PAGES</p>
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
 <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z"/>
 </svg>
 <button
-className="btn dropdown-toggle"
+className="btn dropdown-toggle fw-normal"
 type="button"
 data-bs-toggle="dropdown"
 aria-expanded="false"
@@ -157,7 +157,7 @@ Pages
 <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"/>
 </svg>
 <button
-className="btn dropdown-toggle"
+className="btn dropdown-toggle fw-normal"
 type="button"
 data-bs-toggle="dropdown"
 aria-expanded="false"
@@ -168,19 +168,19 @@ Authentication
 <path d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2z"/>
 </svg>
 <button
-className="btn dropdown-toggle"
+className="btn dropdown-toggle fw-normal"
 type="button"
 data-bs-toggle="dropdown"
 aria-expanded="false"
 >
 Layouts
 </button><br/>
-<p className="dash-text mt-5">UI-COMPONENTS</p>
+<p className="text-muted mt-5">UI-COMPONENTS</p>
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
 <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
 </svg>
 <button
-className="btn dropdown-toggle"
+className="btn dropdown-toggle fw-normal"
 type="button"
 data-bs-toggle="dropdown"
 aria-expanded="false"
