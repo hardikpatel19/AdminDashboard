98033c2809a06f1911a4be483b303e9744f578c8
hardik <hardikpatel1903@gmail.com>
2024-12-19 11:46:06 +0530

    Your commit message here

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -41,7 +41,7 @@ const Sidebar = ({ toggle }) => {
     </li>
       </ul>
       <p className="text-muted">APPS</p>
-      <button className="btn fw-normal">Calender</button><br/>
+      <button className="btn dash-span fw-normal">Calender</button><br/>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
         <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
       </svg><button className="btn btn-text fw-normal">Chat</button><br/>
